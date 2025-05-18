9fde7e596515480f939e4ae86eccccb57d5754f5
Raghavendra Galeppa <raghavendra@vaultedge.com>
2025-05-19 02:34:43 +0530

    Add new sync script

---
+++ sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cp -r ~/.config/fish .
+cp -r ~/.config/nvim .
+cp -r ~/.config/wlogout .
+cp -r ~/.config/waybar .
+cp -r ~/.config/sway .
+
+cp ~/.bashrc .
+cp ~/.bash_aliases .
+cp ~/.vimrc .
+

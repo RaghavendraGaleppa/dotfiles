1c93f998987a3df9fb0d2c65b8ac943a326bbda3
Raghavendra Galeppa <raghavendra@vaultedge.com>
2025-05-23 21:00:53 +0530

    BOOM

--- sync.sh
+++ sync.sh
@@ -7,6 +7,7 @@ cp -r ~/.config/sway .config/
 cp -r ~/.config/alacritty .config/
 cp -r ~/.config/xdg-desktop-portal/ .config/
 cp -r ~/.config/rofi .config/
+cp -r ~/.config/cava .config/
 
 cp ~/.bashrc .
 cp ~/.bash_aliases .
